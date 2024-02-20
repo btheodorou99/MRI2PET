387368fcc3e99fd2149f17171c694432c34473c9
Theodorou <theodoroubp@NIAIRP-02201886.nih.gov>
2024-02-20 11:07:36 -0600

    Adjusting job resources

--- jobs/utils_scalePretrain.sh
+++ jobs/utils_scalePretrain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=72:00:00
 #SBATCH --nodes=1
-#SBATCH --mem=512g
+#SBATCH --mem=247g
 #SBATCH --cpus-per-task=16
 #SBATCH --job-name=scalePretrain
 
--- jobs/utils_styleTransfer.sh
+++ jobs/utils_styleTransfer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --time=72:00:00
 #SBATCH --nodes=1
-#SBATCH --mem=512g
-#SBATCH --cpus-per-task=16
+#SBATCH --mem=247g
+#SBATCH --cpus-per-task=24
 #SBATCH --job-name=styleTransfer
 
 module load python
