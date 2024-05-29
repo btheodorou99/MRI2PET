9e25beb2214abf28c5c3e6bffd4c7501148562eb
Theodorou <theodoroubp@NIAIRP-02201886.nih.gov>
2024-05-29 14:01:07 -0400

    Style transfer job

--- jobs/utils_styleTransfer.sh
+++ jobs/utils_styleTransfer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=4:00:00
+#SBATCH --time=8:00:00
 #SBATCH --nodes=1
-#SBATCH --mem=24g
+#SBATCH --mem=64g
 #SBATCH --cpus-per-task=16
 #SBATCH --job-name=styleTransfer
 #SBATCH --array=0-25
