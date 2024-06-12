4fedb8286d63ab50aafa9d67131a5ed1fd05c211
Theodorou <theodoroubp@NIAIRP-02201886.nih.gov>
2024-06-12 09:23:53 -0400

    Adding back in MRI conditioning

--- src/models/simpleDiffusion.py
+++ src/models/simpleDiffusion.py
@@ -1,9 +1,49 @@
 import math
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+class ImageEncoder(nn.Module):
+    def __init__(self, config):
+        super(ImageEncoder, self).__init__()
+        self.n_channels = config.n_mri_channels
+        self.image_dim = config.mri_image_dim
+        self.target_dim = config.pet_image_dim
+
+        self.conv1 = nn.Conv2d(self.n_channels, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.flat_dim = 256 * (self.image_dim // 16) * (self.image_dim // 16)
+        self.fc1 = nn.Linear(self.flat_dim, config.embed_dim)
+        self.fc2 = nn.Linear(config.embed_dim, config.embed_dim)
+        
+    def forward(self, x):        
+        # Convolution + ReLU + MaxPooling
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        
+        x = F.relu(self.conv3(x))
+        x = F.max_pool2d(x, 2)
+        
+        x = F.relu(self.conv4(x))
+        x = F.max_pool2d(x, 2)
+
+        # Flattening the output
+        x = x.view(-1, self.flat_dim)
+
+        # Passing through the fully connected layers
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        x = x[:, :, None, None].expand(-1, -1, self.target_dim, self.target_dim)
+        return x
 
 class DiffusionModel(nn.Module):
-    def __init__(self, config, final=False, noise=False, timestep=0):
+    def __init__(self, config, final=False, noise=False, timestep=0, mri=False):
         super(DiffusionModel, self).__init__()
         self.num_timesteps = config.num_timesteps
         self.beta = torch.linspace(config.beta_start, config.beta_end, self.num_timesteps)
@@ -15,7 +55,11 @@ class DiffusionModel(nn.Module):
         self.final = final
         self.noise = noise
         self.timestep = timestep
-        self.input_dim = config.n_pet_channels if self.timestep == 0 else config.n_pet_channels + self.embed_dim
+        self.mri = mri
+        self.input_dim = config.n_pet_channels
+        if self.timestep > 0 or self.mri: self.input_dim += self.embed_dim
+        if mri:
+            self.mri_emb = ImageEncoder(config)
         if self.timestep == 3:
             self.time_emb = nn.Sequential(nn.Linear(self.embed_dim, 64), nn.ReLU(), nn.Linear(64, 64), nn.ReLU(), nn.Linear(64, self.embed_dim))
 
@@ -35,7 +79,8 @@ class DiffusionModel(nn.Module):
         emb = torch.exp(torch.arange(half_dim, device=t.device) * -emb)
         emb = t[:, None] * emb[None, :]
         emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
-        emb = self.time_emb(emb)
+        if self.timestep == 3:
+            emb = self.time_emb(emb)
         emb = emb[:, :, None, None].expand(-1, -1, self.image_dim, self.image_dim)
         return emb
     
@@ -58,12 +103,20 @@ class DiffusionModel(nn.Module):
         if self.noise:
             noise.clamp_(-1, 1)
         x_noisy = sqrt_alpha_hat * x + sqrt_one_minus_alpha_hat * noise
-        if self.timestep == 1:
-            x_noisy = torch.cat((x_noisy, self.timestep_embedding1(t)), dim=1)
-        elif self.timestep == 2:
-            x_noisy = torch.cat((x_noisy, self.timestep_embedding2(t)), dim=1)
-        elif self.timestep == 3:
-            x_noisy = torch.cat((x_noisy, self.timestep_embedding1(t)), dim=1)
+        if self.timestep > 0:
+            if self.timestep == 1:
+                emb = self.timestep_embedding1(t)
+            elif self.timestep == 2:
+                emb = self.timestep_embedding2(t)
+            elif self.timestep == 3:
+                emb = self.timestep_embedding1(t)
+            if self.mri:
+                emb += self.mri_emb(context)
+            x_noisy = torch.cat((x_noisy, emb), dim=1)
+        elif self.mri:
+            emb = self.mri_emb(context)
+            x_noisy = torch.cat((x_noisy, emb), dim=1)
+
         x_pred = self.encoder(x_noisy)
         if self.final:
             x_pred = 2 * torch.tanh(x_pred)
