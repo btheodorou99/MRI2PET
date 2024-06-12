11cf45fb07ba3320edf9b30da7fb592a8375d8c1
Theodorou <theodoroubp@NIAIRP-02201886.nih.gov>
2024-06-12 12:06:18 -0400

    Fix Model

--- src/models/diffusionModelFixed.py
+++ src/models/diffusionModelFixed.py
@@ -50,50 +50,68 @@ class RMSNorm(nn.Module):
     def forward(self, x):
         return F.normalize(x, dim = 1) * self.g * (x.shape[1] ** 0.5)
     
+# class LinearAttention(nn.Module):
+#     def __init__(
+#         self,
+#         dim,
+#         heads = 4,
+#         dim_head = 32,
+#         num_mem_kv = 4
+#     ):
+#         super().__init__()
+#         self.scale = dim_head ** -0.5
+#         self.heads = heads
+#         hidden_dim = dim_head * heads
+
+#         self.norm = RMSNorm(dim)
+#         self.to_qkv = nn.Conv2d(dim, hidden_dim * 3, 1, bias = False)
+
+#         self.mem_kv = nn.Parameter(torch.randn(2, heads, dim_head, num_mem_kv))
+
+#         self.to_out = nn.Sequential(
+#             nn.Conv2d(hidden_dim, dim, 1),
+#             RMSNorm(dim)
+#         )
+
+#     def forward(self, x):
+#         b, _, h, w = x.shape
+
+#         x = self.norm(x)
+
+#         qkv = self.to_qkv(x).chunk(3, dim = 1)
+#         q, k, v = map(lambda t: rearrange(t, 'b (h c) x y -> b h c (x y)', h = self.heads), qkv)
+
+#         mk, mv = map(lambda t: repeat(t, 'h c n -> b h c n', b = b), self.mem_kv)
+#         k, v = map(partial(torch.cat, dim = -1), ((mk, k), (mv, v)))
+
+#         q = q.softmax(dim = -2)
+#         k = k.softmax(dim = -1)
+
+#         q = q * self.scale
+
+#         context = torch.einsum('b h d n, b h e n -> b h d e', k, v)
+
+#         out = torch.einsum('b h d e, b h d n -> b h e n', context, q)
+#         out = rearrange(out, 'b h c (x y) -> b (h c) x y', h = self.heads, x = h, y = w)
+#         return self.to_out(x)
+    
 class LinearAttention(nn.Module):
-    def __init__(
-        self,
-        dim,
-        heads = 4,
-        dim_head = 32,
-        num_mem_kv = 4
-    ):
+    def __init__(self, dim, heads=4, dim_head=32):
         super().__init__()
-        self.scale = dim_head ** -0.5
         self.heads = heads
         hidden_dim = dim_head * heads
-
-        self.norm = RMSNorm(dim)
-
-        self.mem_kv = nn.Parameter(torch.randn(2, heads, dim_head, num_mem_kv))
         self.to_qkv = nn.Conv2d(dim, hidden_dim * 3, 1, bias = False)
-
-        self.to_out = nn.Sequential(
-            nn.Conv2d(hidden_dim, dim, 1),
-            RMSNorm(dim)
-        )
+        self.to_out = nn.Conv2d(hidden_dim, dim, 1)
 
     def forward(self, x):
-        b, _, h, w = x.shape
-
-        x = self.norm(x)
-
-        # qkv = self.to_qkv(x).chunk(3, dim = 1)
-        # q, k, v = map(lambda t: rearrange(t, 'b (h c) x y -> b h c (x y)', h = self.heads), qkv)
-
-        # mk, mv = map(lambda t: repeat(t, 'h c n -> b h c n', b = b), self.mem_kv)
-        # k, v = map(partial(torch.cat, dim = -1), ((mk, k), (mv, v)))
-
-        # q = q.softmax(dim = -2)
-        # k = k.softmax(dim = -1)
-
-        # q = q * self.scale
-
-        # context = torch.einsum('b h d n, b h e n -> b h d e', k, v)
-
-        # out = torch.einsum('b h d e, b h d n -> b h e n', context, q)
-        # out = rearrange(out, 'b h c (x y) -> b (h c) x y', h = self.heads, x = h, y = w)
-        return self.to_out(x)
+        b, c, h, w = x.shape
+        qkv = self.to_qkv(x)
+        q, k, v = rearrange(qkv, 'b (qkv heads c) h w -> qkv b heads c (h w)', heads = self.heads, qkv=3)
+        k = k.softmax(dim=-1)  
+        context = torch.einsum('bhdn,bhen->bhde', k, v)
+        out = torch.einsum('bhde,bhdn->bhen', context, q)
+        out = rearrange(out, 'b heads c (h w) -> b (heads c) h w', heads=self.heads, h=h, w=w)
+        return self.to_out(out)
 
 class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels, mid_channels=None, residual=False):
