3e270a012f0ee209bbaa832828a79e61af514556
Theodorou <theodoroubp@NIAIRP-02201886.nih.gov>
2024-07-31 11:45:29 -0400

    analysis

--- src/utils/PET_ExploreQuality.py
+++ src/utils/PET_ExploreQuality.py
@@ -15,26 +15,23 @@ for f in tqdm(os.listdir(input_dir)):
     if not f.endswith(".nii") or os.path.exists(os.path.join(output_dir, f.replace(".nii", ".png"))):
         continue
 
-    # print(f)
     img = ants.image_read(os.path.join(input_dir, f))
-    # print(img.shape)
     if len(img.shape) == 4:
-        if f == '037_S_4015--2013-04-19--I368085.nii':
-            print(img[:, :, :, 0].max())
-            img = ants.from_numpy(img[:, :, :, 1])
-        else:
-            img = ants.from_numpy(img[:, :, :, 0])
+        img = ants.from_numpy(img[:, :, :, 0])
 
     img = ants.from_numpy(img.numpy().transpose(2, 0, 1))
     depths.append(img.shape[0])
     heights.append(img.shape[1])
     widths.append(img.shape[2])
-    ants.plot(
-        img,
-        nslices=9,
-        title=str(img.shape[0]),
-        filename=f"{output_dir}{f.replace('.nii', '.png')}",
-    )
+    try:
+        ants.plot(
+            img,
+            nslices=9,
+            title=str(img.shape[0]),
+            filename=f"{output_dir}{f.replace('.nii', '.png')}",
+        )
+    except Exception as e:
+        print(f'Error with {f}: {img.shape}, {img.numpy().max()}')
 
 print(Counter(depths))
 print(Counter(heights))
