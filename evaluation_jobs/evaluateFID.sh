57f604ec1751e3550432b360d851b66fc66745b1
Theodorou <theodoroubp@NIAIRP-02201886.nih.gov>
2024-11-05 09:40:59 -0500

    Change GPU to fix path error

--- evaluation_jobs/evaluateFID.sh
+++ evaluation_jobs/evaluateFID.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --mem=64g
 #SBATCH --cpus-per-task=8
 #SBATCH --job-name=frechetInceptionDistance
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:a100:1
+#SBATCH --gres=gpu:p100:1
 #SBATCH --error=evaluateFID.err
 #SBATCH --output=evaluateFID.out
 
