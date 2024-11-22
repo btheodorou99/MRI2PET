309a41a4d07c8e173dd6536b0f3135a88a079f11
Theodorou <theodoroubp@NIAIRP-02201886.nih.gov>
2024-11-22 11:17:58 -0500

    Moving towards PET processing

--- src/utils/PET_PreProcessing.py
+++ src/utils/PET_PreProcessing.py
@@ -13,6 +13,8 @@ output_dir = "/data/CARD_AA/data/ADNI/PET_Nifti_PreProcessed/"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
+template = ants.image_read('./src/data/petTemplate.nii')
+
 # {subject_id}--{date}--{some_ids}.nii
 single_files = []
 problem_files = []
@@ -31,27 +33,34 @@ for niix_file in tqdm(allFiles):
             num_time_points = img.shape[3]
             
             # Register to First on Initial Pass
-            reference_frame = ants.from_numpy(img[:, :, :, 0])
-            initial_registered_frames = [reference_frame.numpy()]
+            reference_frame = ants.slice_image(img, axis=3, idx=0)
+            initial_registered_frames = [reference_frame]
             for i in range(1, num_time_points):
-                moving_frame = img[:, :, :, i]
-                registration = ants.registration(fixed=reference_frame, moving=ants.from_numpy(moving_frame), type_of_transform='Rigid')
-                initial_registered_frames.append(registration['warpedmovout'].numpy())
+                registration = ants.registration(fixed=reference_frame, moving=ants.slice_image(img, axis=3, idx=i), type_of_transform='Rigid')
+                initial_registered_frames.append(registration['warpedmovout'])
 
             # Register to Mean on Second Pass
-            reference_frame = ants.from_numpy(np.mean(np.array(initial_registered_frames), axis=0))
+            reference_frame = ants.from_numpy(np.mean(np.array([f.numpy() for f in initial_registered_frames]), axis=0), origin=reference_frame.origin, spacing=reference_frame.spacing, direction=reference_frame.direction)
             final_registered_frames = []
             for i in range(0, num_time_points):
                 moving_frame = initial_registered_frames[i]
-                registration = ants.registration(fixed=reference_frame, moving=ants.from_numpy(moving_frame), type_of_transform='Rigid')
+                registration = ants.registration(fixed=reference_frame, moving=moving_frame, type_of_transform='Rigid')
                 final_registered_frames.append(registration['warpedmovout'].numpy())
 
-            img = ants.from_numpy(np.mean(np.array(final_registered_frames), axis=0))
+            img = ants.from_numpy(np.mean(np.array(final_registered_frames), axis=0), origin=reference_frame.origin, spacing=reference_frame.spacing, direction=reference_frame.direction)
 
         # Disregard images that are just a single slice
         if img.shape[2] == 1:
             single_files.append(niix_file)
             continue
+
+        # Register to PET template
+        img = ants.registration(fixed=template, moving=img, type_of_transform="Affine")['warpedmovout']
+
+        # Strip the skull
+
+        # Reorient the direction
+        
 
         img = ants.resample_image(img, (config.pet_image_dim, config.pet_image_dim, config.n_pet_channels), use_voxels=True, interp_type=3)
         img = ants.utils.convert_nibabel.to_nibabel(img)
