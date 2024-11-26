733b9f3a866e53a5b5b3a50ba0e9dd02428a07c0
Theodorou <theodoroubp@NIAIRP-02201886.nih.gov>
2024-11-26 16:36:30 -0700

    Final run

--- utils_jobs/utils_styleTransfer.sh
+++ utils_jobs/utils_styleTransfer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --time=4:00:00
+#SBATCH --time=48:00:00
 #SBATCH --nodes=1
 #SBATCH --mem=8g
 #SBATCH --cpus-per-task=4
 #SBATCH --job-name=styleTransfer
-#SBATCH --array=0-4
+#SBATCH --array=1-50
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:k80:1,lscratch:100
+#SBATCH --gres=gpu:k80:1,lscratch:50
 
 export TMPDIR=/lscratch/$SLURM_JOB_ID
 export OMP_NUM_THREADS=1
