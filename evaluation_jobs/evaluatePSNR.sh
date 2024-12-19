26238efd74bd53b329ab776ff2eb2a5f78afc7a0
Theodorou <theodoroubp@NIAIRP-02201886.nih.gov>
2024-12-19 16:49:21 -0700

    fix bootstrap

--- evaluation_jobs/evaluatePSNR.sh
+++ evaluation_jobs/evaluatePSNR.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --mem=64g
 #SBATCH --cpus-per-task=8
-#SBATCH --job-name=structuralSimilarityIndex
+#SBATCH --job-name=peak_signal_to_noise_ratio
 #SBATCH --error=evaluatePSNR.err
 #SBATCH --output=evaluatePSNR.out
 
