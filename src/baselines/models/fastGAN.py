d437da2909136ead76ee12a10fbae986b1824573
Brandon Theodorou <brandontheodorou@Brandons-MacBook-Pro.local>
2024-12-27 09:06:12 -0700

    Test

--- src/baselines/models/fastGAN.py
+++ src/baselines/models/fastGAN.py
@@ -122,40 +122,33 @@ def UpBlockComp(in_planes, out_planes):
 class Generator(nn.Module):
     def __init__(self, config):
         super(Generator, self).__init__()
-        ngf=64
         nz=config.z_dim 
         nc=config.n_pet_channels
         im_size=config.pet_image_dim
-
-        nfc_multi = {4:16, 8:8, 16:4, 32:2, 64:2, 128:1, 256:0.5, 512:0.25, 1024:0.125}
-        nfc = {}
-        for k, v in nfc_multi.items():
-            nfc[k] = int(v*ngf)
-
         self.im_size = im_size
 
         self.context_emb = ImageEncoder(config, is_mri=True)
-        self.init = InitLayer(nz+config.embed_dim, channel=nfc[4])
+        self.init = InitLayer(nz+config.embed_dim, channel=8)
                                 
-        self.feat_8   = UpBlockComp(nfc[4], nfc[8])
-        self.feat_16  = UpBlock(nfc[8], nfc[16])
-        self.feat_32  = UpBlockComp(nfc[16], nfc[32])
-        self.feat_64  = UpBlock(nfc[32], nfc[64])
-        self.feat_128 = UpBlockComp(nfc[64], nfc[128])  
-        self.feat_256 = UpBlock(nfc[128], nfc[256]) 
+        self.feat_8   = UpBlockComp(8, 16)
+        self.feat_16  = UpBlock(16, 32)
+        self.feat_32  = UpBlockComp(32, 32)
+        self.feat_64  = UpBlock(32, 32)
+        self.feat_128 = UpBlockComp(32, 16)  
+        self.feat_256 = UpBlock(16, 8) 
 
-        self.se_64  = SEBlock(nfc[4], nfc[64])
-        self.se_128 = SEBlock(nfc[8], nfc[128])
-        self.se_256 = SEBlock(nfc[16], nfc[256])
+        self.se_64  = SEBlock(8, 32)
+        self.se_128 = SEBlock(16, 16)
+        self.se_256 = SEBlock(8, 8)
 
-        self.to_128 = conv3d(nfc[128], nc, 1, 1, 0, bias=False) 
-        self.to_big = conv3d(nfc[im_size], nc, 3, 1, 1, bias=False) 
+        self.to_128 = conv3d(16, nc, 1, 1, 0, bias=False) 
+        self.to_big = conv3d(8, nc, 3, 1, 1, bias=False) 
         
         if im_size > 256:
-            self.feat_512 = UpBlockComp(nfc[256], nfc[512]) 
-            self.se_512 = SEBlock(nfc[32], nfc[512])
+            self.feat_512 = UpBlockComp(8, 8) 
+            self.se_512 = SEBlock(8, 8)
         if im_size > 512:
-            self.feat_1024 = UpBlock(nfc[512], nfc[1024])  
+            self.feat_1024 = UpBlock(8, 8)  
         
     def forward(self, input, context):
         context = context.unsqueeze(1)
@@ -169,16 +162,8 @@ class Generator(nn.Module):
         feat_64  = self.se_64(feat_4, feat_64)
 
         feat_128 = self.se_128( feat_8, self.feat_128(feat_64) )
-
         feat_256 = self.se_256( feat_16, self.feat_256(feat_128) )
-
-        if self.im_size == 256:
-            return [self.to_big(feat_256), self.to_128(feat_128)]
-        
         feat_512 = self.se_512( feat_32, self.feat_512(feat_256) )
-        if self.im_size == 512:
-            return [self.to_big(feat_512), self.to_128(feat_128)]
-
         feat_1024 = self.feat_1024(feat_512)
 
         im_128 = torch.tanh(self.to_128(feat_128))
