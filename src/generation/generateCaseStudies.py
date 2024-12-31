63a452c1670a177b0bd12cd3b5f01113ca18c8fa
Theodorou <theodoroubp@NIAIRP-02201886.nih.gov>
2024-12-31 09:26:26 -0700

    Case Study

--- src/generation/generateCaseStudies.py
+++ src/generation/generateCaseStudies.py
@@ -21,6 +21,31 @@ device = torch.device(f"cuda:{cudaNum}" if torch.cuda.is_available() else "cpu")
 if torch.cuda.is_available():
     torch.cuda.manual_seed_all(SEED)
 
+interesting_subjects = {
+    ('051_S_1072', '2012-02-29'): 'MCI_WeirdShape',
+    ('002_S_4521', '2016-03-29'): 'AD_BetterResolutionwSulkusOnBottom',
+    ('094_S_2216', '2015-03-17'): 'AD_LongitudinalProgression',
+    ('135_S_4722', '2012-06-05'): 'MCI_LongitudinalProgression',
+    ('114_S_6524', '2020-12-08'): 'CN_Atrophy',
+    ('020_S_5203', '2015-05-27'): 'CN_ImageQuality',
+    ('129_S_6784', '2019-09-25'): 'AD_LongitudinalProgression',
+    ('129_S_6784', '2021-08-20'): 'AD_LongitudinalProgression',
+    ('041_S_1010', '2010-12-10'): 'AD_GyriBetterAndWorse',
+    ('037_S_5162', '2013-05-30'): 'AD_SulksTopAndBottom',
+    ('021_S_6914', '2021-03-24'): 'CN_ThingOnLeft',
+    ('126_S_5214', '2013-08-15'): 'MCI_Atrophy',
+    ('036_S_4562', '2012-04-12'): 'MCI_Gyri',
+    ('037_S_4030', '2015-05-27'): 'AD_VerySimilar',
+    ('135_S_4722', '2021-06-18'): 'MCI_LongitudinalProgression',
+    ('002_S_5178', '2017-06-05'): 'CN_BrightGyriAroundGap',
+    ('005_S_0610', '2014-12-09'): 'CN_ImageProcess',
+    ('035_S_4785', '2019-12-10'): 'MCI_GyriClear',
+    ('126_S_4891', '2018-08-22'): 'AD_Atrophy',
+    ('094_S_2216', '2013-03-12'): 'AD_LongitudinalProgression',
+    ('021_S_4744', '2012-06-07'): 'MCI_GyriRightSide',
+    ('099_S_6025', '2021-11-04'): 'CN_DeepSulkus'
+}    
+
 def getID(path):
     fname = path.split("/")[-1]
     subject = fname.split("--")[0]
@@ -31,11 +56,8 @@ def getDate(path):
     date = fname.split("--")[1]
     return date
 
-adni_labels = pd.read_csv("./src/data/DXSUM_PDXCONV.csv")
-adni_labels = {row.PTID: row.DIAGNOSIS for row in adni_labels.itertuples()}
-adni_labels = {p: int(l - 1) for p, l in adni_labels.items() if l == l} # 0: CN, 1: MCI, 2: AD
-
 test_dataset = pickle.load(open("./src/data/testDataset.pkl", "rb"))
+test_dataset = [x for x in test_dataset if (getID(x[1]), getDate(x[1])) in interesting_subjects]
 global_mean = pickle.load(open("./src/data/globalMean.pkl", "rb"))
 global_std = pickle.load(open("./src/data/globalStd.pkl", "rb"))
 
@@ -97,17 +119,16 @@ def expand_slices(array, target_shape):
 def save_slice_plots(mri, real, synthetic, path_prefix, key):
     # Array: (Height, Width, Slices)
     # Directions: Axial, Sagittal, Coronal
-    mri = resize_array(mri, real.shape[0])
 
     # Axial
-    for i in range(real.shape[2] // 2, (real.shape[2] // 2) + 1):
+    for i in range(real.shape[2] // 3, real.shape[2] * 2 // 3):
         slice_mri = mri[:, :, i]
         slice_real = real[:, :, i]
         slice_synthetic = synthetic[:, :, i]
         combined_slice = np.hstack((slice_mri, slice_real, slice_synthetic))
         if combined_slice.max() > 0.05:
             plt.imsave(
-                os.path.join(path_prefix, f"Axial_{key}_{i}.png"),
+                os.path.join(path_prefix, f"{key}_{i}.png"),
                 combined_slice,
                 cmap="gray",
             )
@@ -158,9 +179,9 @@ for i in tqdm(range(0, len(test_dataset), config.batch_size)):
         fpath = test_dataset[i+j][1]
         subject = getID(fpath)
         date = getDate(fpath)
-        ad_status = adni_labels.get(subject, "Unknown")
+        note = interesting_subjects[(subject, date)]
         real_pet = tensor_to_numpy(real_images[j].cpu())
         real_mri = resize_array(sample_contexts[j].cpu().clone().numpy().transpose((1, 2, 0)), real_pet.shape[0])
         real_mri = align_mri(real_mri, real_pet)
         generated_pet = tensor_to_numpy(generated_batch[j])
-        save_slice_plots(real_mri, real_pet, generated_pet, f"./src/results/case_study_samples/", f"{subject}_{date}_{ad_status}")
+        save_slice_plots(real_mri, real_pet, generated_pet, f"./src/results/case_study_samples/", f"{subject}_{date}_{note}")
